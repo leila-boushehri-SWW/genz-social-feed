3a6c0280929e3bb96d866d48e66e0cee9faf733c
leila-boushehri-SWW <120033581+leila-boushehri-SWW@users.noreply.github.com>
2025-08-19 17:11:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AveryChat from "./AveryChat.jsx";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GenZChatDemo from "./GenZChatDemo.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AveryChat />
-  </React.StrictMode>,
-)
+    <GenZChatDemo />
+  </React.StrictMode>
+);
