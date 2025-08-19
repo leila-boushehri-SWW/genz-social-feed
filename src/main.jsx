325676174f6650fd6bf07b88e26451946395efbb
leila-boushehri-SWW <120033581+leila-boushehri-SWW@users.noreply.github.com>
2025-08-19 16:44:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GenZChatDemo from './GenZChatDemo.jsx'
+import AveryChat from "./AveryChat.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GenZChatDemo />
+    <AveryChat />
   </React.StrictMode>,
 )
